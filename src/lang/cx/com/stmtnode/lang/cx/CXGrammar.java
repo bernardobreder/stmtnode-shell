a52561fd28835c934368b3a11a0ca487980ecd83
Bernardo Breder <bernardobreder@gmail.com>
2018-01-28 22:28:35 -0200

    .

---
+++ src/lang/cx/com/stmtnode/lang/cx/CXGrammar.java
@@ -0,0 +1,12 @@
+package com.stmtnode.lang.cx;
+
+import com.stmtnode.lang.compiler.Grammar;
+import com.stmtnode.lang.compiler.Token;
+
+public class CXGrammar extends Grammar {
+
+	public CXGrammar(Token[] tokens) {
+		super(tokens);
+	}
+
+}
