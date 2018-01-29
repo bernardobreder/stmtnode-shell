d2ff496e500483bb5c664b783d7db5bf105b6a47
Bernardo Breder <bernardobreder@gmail.com>
2018-01-28 22:31:00 -0200

    .

--- src/watch/com/stmtnode/watch/Main.java
+++ src/watch/com/stmtnode/watch/Main.java
@@ -2,18 +2,33 @@ package com.stmtnode.watch;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.stmtnode.module.ModuleData;
+import com.stmtnode.module.ModuleRoot;
 
 public class Main {
 
 	static Log logger = new Log();
 
-	public static void main(String[] args) {
+	public Main() {
 		TaskQueue queue = new TaskQueue();
 		Path inDir = Paths.get("mod");
 		Path outDir = Paths.get("out");
-		new ModuleWatch(inDir, paths -> {
-			System.out.println(paths);
-		}).start();
+		new ModuleWatch(inDir, this::execute).start();
+	}
+
+	private void execute(ModuleRoot root) {
+		List<ModuleData> modules = root.modules;
+		for (ModuleData module : modules) {
+			module.contents.entrySet().stream() //
+					.collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue()));
+		}
+	}
+
+	public static void main(String[] args) {
+		new Main();
 	}
 
 }
